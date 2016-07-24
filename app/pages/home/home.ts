220bc406e9bb7248158ac16aabe156c45a9712a0
Julian <ju-fa-beck@t-online.de>
2016-07-24 21:45:49 +0200

    added custom notification sound

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -12,48 +12,50 @@ import {Subject} from 'rxjs/Subject';
 export class HomePage {
   public running = false
   public interval
+  public timer
   public minutes = 5
   public time = " "
   public pauser = new Subject();
+  public timerInterval
   constructor(private navController: NavController) {
 
   }
 
   onClick() {
     if (!this.running) {
-      this.interval = Observable.interval(50).subscribe(this.callNotificaton)
-      this.running = !this.running
-
-    } else {
+        //this.interval = Observable.interval(10000).subscribe(this.callNotificaton);
+      this.startTimer(10);
       this.running = !this.running;
 
-      this.interval.unsubscribe();
-    }
-    let fiveMinutes = 60 * 5;
+    } else {
 
-    this.startTimer(fiveMinutes);
+      this.timerInterval.unsubscribe();
+
+        this.running = !this.running;
+    }
   }
 
 
   callNotificaton() {
     LocalNotifications.schedule({
       text: "Pokemstop Ready",
-
+      sound: 'file://res/sound.mp3'
 
     })
   }
 
   startTimer(duration) {
     let timer = duration, minutes, seconds;
-    Observable.interval(1000).subscribe(() => {
+    this.timerInterval= Observable.interval(1000).subscribe(() => {
       minutes = timer / 60, 10;
       seconds = timer % 60, 10;
 
       minutes = minutes < 10 ? "0" + minutes : minutes;
       seconds = seconds < 10 ? "0" + seconds : seconds;
-      this.time = parseInt(minutes) + " " + seconds;
+      this.time = parseInt(minutes) + ":" + seconds;
 
       if (--timer < 0) {
+        this.callNotificaton();
         timer = duration;
       }
     });
